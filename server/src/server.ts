21f700e391580badbd30cc2c5f3cb65a039a3019
maikaodev <maikao.dev@gmail.com>
2023-05-17 20:45:18 -0300

    feat: config prettier with eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,18 @@
-import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import fastify from 'fastify'
 
-const app = fastify();
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
 
-app.get("/users", async () => {
-  const users = await prisma.user.findMany();
+const app = fastify()
+const prisma = new PrismaClient()
 
-  return users;
-});
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
 
 app
   .listen({
     port: 3333,
   })
-  .then(() => console.log("🚀 HTTP server running on http://localhost:3333"));
+  .then(() => console.log('🚀 HTTP server running on http://localhost:3333'))
